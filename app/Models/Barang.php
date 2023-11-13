cd1c9bf7e6c9a69fe79c4c5c530b7f0069de64d6
ca-kraa <alexandrokingnabil@gmail.com>
2023-11-13 17:15:05 +0800

    Update

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+    protected $table = 'barangs';
+    protected $fillable = ['nama_barang', 'deskripsi_barang', 'harga_barang'];
 }
